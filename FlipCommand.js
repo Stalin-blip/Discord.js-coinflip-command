9c7034a2fba1bb5a43b1b2788be415c316abf794
åµøß <77791528+Stalin-blip@users.noreply.github.com>
2021-02-19 18:35:02 -0500

    Add files via upload

---
+++ FlipCommand.js
@@ -0,0 +1,25 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+
+module.exports = class FlipCommand extends BaseCommand {
+  constructor() {
+    super('flip', 'rand', []);
+  }
+
+  run(client, message, args) {
+    let get_random = function (list) {
+      return list[Math.floor((Math.random() * list.length))];
+    }
+    let list = ['Heads', 'Tails']
+    let emb1 = new Discord.MessageEmbed()
+      .setTitle("Flipping...")
+    let emb2 = new Discord.MessageEmbed()
+      .setThumbnail("https://hattonsoflondon.co.uk/wp-content/uploads/2020/05/steve-smith-Zvvu4zRKijE-unsplash.jpg")
+      .setTitle("Landed on:")
+      .setDescription(`\`\`\`${get_random(list)}\`\`\` `)
+
+    message.channel.send(emb1)
+    setTimeout(() => {
+      message.channel.send(emb2)
+    }, 20);
+  }
+}
